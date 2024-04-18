2d32702d2121cfef433642c7a3c6de9ac209c681
Shrivalli <shrivalli04@gmail.com>
2024-04-18 10:55:50 +0530

    Add project files.

---
+++ Location.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace RabbitMQDemo
+{
+    public class Location
+    {
+        public DateTime Date { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ LocationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQDemo
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] Location location)
+        {
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "locationSampleQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Latitude: " + location.Latitude + ", Longitude: " + location.Longitude + " and Time: " + location.Date;
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "locationSampleQueue",
+                                     basicProperties: null,
+                                     body: body);
+            }
+        }
+    }
+}
+
+
+
